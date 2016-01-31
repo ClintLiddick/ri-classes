970c8c1529faba76b4fa6a52ce2f6473aff9373f
Clint Liddick <clint.liddick@gmail.com>
2016-01-30 21:17:37 -0500

    finished w/ plot output

--- kdc/fk.m
+++ kdc/fk.m
@@ -4,34 +4,34 @@ if ~isvector(q) or length(q) ~= 7
 else
     %disp(q)
     % raw translations
-    base_S   = makehgtform('translate', [.75, .5, 1.0])
-    link_0_b = makehgtform('translate', [.22,.24,.346])
-    link_1_0 = makehgtform('translate', [0,0,0])
-    link_2_1 = makehgtform('translate', [0,0,0])
-    link_3_2 = makehgtform('translate', [.045, 0, .55])
-    link_4_3 = makehgtform('translate', [0,0,0])
-    link_5_4 = makehgtform('translate', [-0.045, 0, .3])
-    link_6_5 = makehgtform('translate', [0,0,0])
-    link_7_6 = makehgtform('translate', [0,0,.06])
-    tool_7   = makehgtform('translate', [0,0,.12])
+    base_S   = makehgtform('translate', [.75, .5, 1.0]);
+    link_0_b = makehgtform('translate', [.22,.24,.346]);
+    link_1_0 = makehgtform('translate', [0,0,0]);
+    link_2_1 = makehgtform('translate', [0,0,0]);
+    link_3_2 = makehgtform('translate', [.045, 0, .55]);
+    link_4_3 = makehgtform('translate', [0,0,0]);
+    link_5_4 = makehgtform('translate', [-0.045, 0, .3]);
+    link_6_5 = makehgtform('translate', [0,0,0]);
+    link_7_6 = makehgtform('translate', [0,0,.06]);
+    tool_7   = makehgtform('translate', [0,0,.12]);
 
     % add rotational transforms
-    link_1_0 = link_1_0*makehgtform('xrotate', -pi/2)
-    link_2_1 = link_2_1*makehgtform('xrotate',  pi/2)
-    link_3_2 = link_3_2*makehgtform('xrotate', -pi/2)
-    link_4_3 = link_4_3*makehgtform('xrotate',  pi/2)
-    link_5_4 = link_5_4*makehgtform('xrotate', -pi/2)
-    link_6_5 = link_6_5*makehgtform('xrotate',  pi/2)
+    link_1_0 = link_1_0*makehgtform('xrotate', -pi/2);
+    link_2_1 = link_2_1*makehgtform('xrotate',  pi/2);
+    link_3_2 = link_3_2*makehgtform('xrotate', -pi/2);
+    link_4_3 = link_4_3*makehgtform('xrotate',  pi/2);
+    link_5_4 = link_5_4*makehgtform('xrotate', -pi/2);
+    link_6_5 = link_6_5*makehgtform('xrotate',  pi/2);
 
     % joint rotations are all about their own frame's z-axis
-    link_0_b = link_0_b*makehgtform('zrotate', q(1))
-    link_1_0 = link_1_0*makehgtform('zrotate', q(2))
-    link_2_1 = link_2_1*makehgtform('zrotate', q(3))
-    link_3_2 = link_3_2*makehgtform('zrotate', q(4))
-    link_4_3 = link_4_3*makehgtform('zrotate', q(5))
-    link_5_4 = link_5_4*makehgtform('zrotate', q(6))
-    link_6_5 = link_6_5*makehgtform('zrotate', q(7))
+    link_0_b = link_0_b*makehgtform('zrotate', q(1));
+    link_1_0 = link_1_0*makehgtform('zrotate', q(2));
+    link_2_1 = link_2_1*makehgtform('zrotate', q(3));
+    link_3_2 = link_3_2*makehgtform('zrotate', q(4));
+    link_4_3 = link_4_3*makehgtform('zrotate', q(5));
+    link_5_4 = link_5_4*makehgtform('zrotate', q(6));
+    link_6_5 = link_6_5*makehgtform('zrotate', q(7));
     
-    tool_base = base_S*link_0_b*link_1_0*link_2_1*link_3_2*link_4_3*link_5_4*link_6_5*tool_7
-    ee_pose = tool_base
+    tool_base = base_S*link_0_b*link_1_0*link_2_1*link_3_2*link_4_3*link_5_4*link_6_5*tool_7;
+    ee_pose = tool_base;
 end
--- kdc/hw1.m
+++ kdc/hw1.m
@@ -1,10 +1,11 @@
 function ee_points = hw1(filename)
-data = dlmread(filename)
-%disp data
-data_length = length(data(:,1))
-ee_points = zeros(3,data_length)
+data = dlmread(filename);
+data_length = length(data(:,1));
+%data_length = 1000
+ee_points = zeros(3,data_length);
 for i = [1:data_length]
-    ee_pose = fk(data(i,:))
-    ee_points(:,i) = ee_pose(1:3,4)
+    ee_pose = fk(data(i,:));
+    ee_points(:,i) = ee_pose(1:3,4);
 end
+scatter(ee_points(1,1:data_length),ee_points(2,1:data_length),4,'blue','filled');
 end
