8bcefe69cd07aab155795b3d5775c7933d9f5ba8
Clint Liddick <clint.liddick@gmail.com>
2016-01-30 22:01:20 -0500

    print results and do plots optionally with flags

--- kdc/hw1.m
+++ kdc/hw1.m
@@ -1,4 +1,4 @@
-function ee_points = hw1(filename)
+function ee_points = hw1(filename,print_results,plot2d,plot3d)
 data = dlmread(filename);
 data_length = length(data(:,1));
 ee_points = zeros(3,data_length);
@@ -6,9 +6,15 @@ for i = [1:data_length]
     ee_pose = fk(data(i,:));
     ee_points(:,i) = ee_pose(1:3,4);
 end
-% uncomment to generate 2d plot
-%scatter(ee_points(1,1:data_length),ee_points(2,1:data_length),4,'blue','filled');
 
-% uncomment to generate 3d plot
-%scatter3(ee_points(1,1:data_length),ee_points(2,1:data_length),ee_points(3,1:data_length),4,'blue','filled');
+
+if nargin > 1 & print_results
+    dlmwrite('results.txt',transpose(ee_points), 'delimiter', ' ');
+end
+
+if nargin > 2 & plot2d
+    scatter(ee_points(1,1:data_length),ee_points(2,1:data_length),4,'blue','filled');
+elseif nargin > 3 & plot3d
+    scatter3(ee_points(1,1:data_length),ee_points(2,1:data_length),ee_points(3,1:data_length),4,'blue','filled');
+end
 end
